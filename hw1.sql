134f6c879aaa8482626467693db270a212fe498a
kelseyobrien <kelsey.obrien524@gmail.com>
2013-09-10 14:34:57 -0400

    First homework with better organization

--- hw1.sql
+++ hw1.sql
@@ -2,22 +2,55 @@
 --Homework 1
 --9/9/13
 
-SELECT * FROM customers;
+--1
+SELECT * 
+FROM customers;
 
-SELECT name, city FROM agents WHERE name='Smith';
+--2
+SELECT name, city 
+FROM agents 
+WHERE name='Smith';
 
-SELECT pid, name, quantity FROM products WHERE priceUSD>1.25;
+--3
+SELECT pid, name, quantity 
+FROM products
+WHERE priceUSD>1.25;
 
-SELECT ordno, aid FROM orders;
+--4
+SELECT ordno, aid 
+FROM orders;
 
-SELECT name, city FROM customers WHERE city<>'Dallas';
+--5
+SELECT name, city 
+FROM customers 
+WHERE city<>'Dallas';
 
-SELECT name FROM agents WHERE city='New York' OR city='Newark';
+--6
+SELECT name 
+FROM agents 
+WHERE city='New York' 
+OR city='Newark';
 
-SELECT * FROM products WHERE city<>'New York' AND city<>'Newark' AND priceUSD<=1.00;
+--7
+SELECT * 
+FROM products 
+WHERE city<>'New York' 
+AND city<>'Newark' 
+AND priceUSD<=1.00;
 
-SELECT * FROM orders WHERE mon='jan' OR mon='mar';
+--8
+SELECT * 
+FROM orders 
+WHERE mon='jan' 
+OR mon='mar';
 
-SELECT * FROM orders WHERE mon='feb' AND dollars<100.00;
+--9
+SELECT * 
+FROM orders 
+WHERE mon='feb' 
+AND dollars<100.00;
 
-SELECT * FROM orders WHERE cid='c005';
+--10
+SELECT * 
+FROM orders 
+WHERE cid='c005';
